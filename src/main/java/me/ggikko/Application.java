271c8dfaddad3fc520f08d90bdaaa50c7fe0e058
ggikko <ggikko@hotmail.com>
2016-03-08 13:05:22 +0900

    commerce hompage, init setting :)

---
+++ src/main/java/me/ggikko/Application.java
@@ -0,0 +1,20 @@
+package me.ggikko;
+
+/**
+ * Created by Park Ji Hong, ggikko.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by Park Ji Hong, ggikko.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
